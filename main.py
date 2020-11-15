d1e7c00a6dbc603816e4aa5f080209832d571b09
delta6862 <delta6862@protonmail.com>
2020-11-15 15:17:01 +0100

    update

--- main.py
+++ main.py
@@ -6,17 +6,20 @@ from collection import *
 from setup import registry_setup
 from setup import *
 
+# Import healthcheck agents
+from healthcheck import registry_healthcheck
+from healthcheck import *
+
 import configparser
 import os
 
 config = configparser.ConfigParser()
 config.read('8k-configs-go.brrr')
 
-privkey = config['General']['key_file']
-
-# Run each collection agent
-for fortress in config['Blue-Fortress-IP']:
-    for m in registry_collection.collection_mods:
-        m(config['Blue-Fortress-IP'][fortress], fortress, privkey)
+def collect():
+    # Run each collection agent
+    for fortress in config['Blue-Fortress-IP']:
+        for m in registry_collection.collection_mods:
+            m(config['Blue-Fortress-IP'][fortress], fortress, config['General']['key_file'])
 
         
